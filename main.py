7ac49b4e4c13796d90673e3252c58732d0ebec61
Zenitsu-god <thunderzenitsugod@gmail.com>
2026-02-15 23:35:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import pandas as pd
 from sklearn.tree import DicisionTreeRegressor
+
+iowa_file_path =""
+
+home_data = pd.read_csv(iowa_file_path)
+
+home_data.describe()
+
+
+y = home_data.SalesPrice
+feature_columns = []
+
+X=home_data[feature_columns]
+
+iowa_model = DicisionTreeRegressor(random_state =1)
+
+iowa_model.fit(X,y)
+
+print(iowa_model.predict(X))
